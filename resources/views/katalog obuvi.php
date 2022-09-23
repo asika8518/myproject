b0bf3f548d91b18aec1694d77b8464bf9fc21429
AnnaBelle <69844021+SushkovaAnna@users.noreply.github.com>
2022-09-23 10:53:46 +0600

    Add files via upload

---
+++ resources/views/katalog obuvi.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="C:\Users\User\Desktop\magazin\css\style_katalog.css">
+    <link rel="stylesheet" href="C:\Users\User\Desktop\magazin\css\bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+    <title>Каталог обуви</title>
+</head>
+<body>
+    <div class=".container-fluid" style="background-color: rgb(235, 227, 222);">
+    <div class="top">
+        <h1 class="katalog">Каталог обуви:</h1>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\krosi.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Кроссовки</h5>
+              </a>
+                  <p class="card-text">Цена: 20000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>   
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\krosi2.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Кроссовки</h5>
+              </a>
+                  <p class="card-text">Цена: 22000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\krosi3.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Кроссовки</h5>
+              </a>
+                  <p class="card-text">Цена: 21000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+        </div>
+      </div>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px; margin-left: 60px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\krosi4.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Кроссовки</h5>
+              </a>
+                  <p class="card-text">Цена: 25000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\tufli.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Туфли</h5>
+              </a>
+                  <p class="card-text">Цена: 15000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px; margin-left: 75px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 180px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\obuv\krosi5.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Кроссовки</h5>
+              </a>
+                  <p class="card-text">Цена: 17000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+        </div>
+      </div>
+</body>
+</html>
---
+++ resources/views/katalog platev.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="C:\Users\User\Desktop\magazin\css\style_katalog.css">
+    <link rel="stylesheet" href="C:\Users\User\Desktop\magazin\css\bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+    <title>Каталог платьев</title>
+</head>
+<body>
+    <div class=".container-fluid" style="background-color: rgb(235, 227, 222);">
+    <div class="top">
+        <h1 class="katalog">Каталог платьев:</h1>
+    </div>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 170px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 12000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>   
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate2.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 15000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate3.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 21000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+        </div>
+      </div>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px; margin-left: 60px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 200px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate4.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 20000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 150px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate5.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 17000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 18rem; height: 400px; margin-left: 75px;">
+              <a class="podrobnee" href="C:\Users\User\Desktop\magazin\podrobnee.php">
+                <img style="width: 180px; height: 200px;" src="C:\Users\User\Desktop\magazin\img\platya\plate6.jpg" class="card-img-top" alt="error">
+                <div class="card-body">
+                  <h5 class="card-title">Платье</h5>
+              </a>
+                  <p class="card-text">Цена: 18000тг</p>
+                  <a href="C:\Users\User\Desktop\magazin\korzina.php" style="background-color: rgb(204, 163, 135); border-color: rgb(204, 163, 135);" class="btn btn-primary">Купить</a>
+                </div>
+              </div>
+        </div>
+      </div>
+</body>
+</html>
